7789112f22526d778e1b1c129a659ceda6059700
zidian <59871768+fenghualuo267@users.noreply.github.com>
2020-07-15 08:57:19 +0800

    Update dictionary_learning_driver.m

--- dictionary_learning_driver.m
+++ dictionary_learning_driver.m
@@ -4,24 +4,24 @@ close all;
 
 % load an image, convert it to grayscale, linearize it
 filename = 'lena.jpg';
-I = im2double( rgb2gray( imread( filename ) ) ).^2.4;
+I = im2double( rgb2gray( imread( filename ) ) ).^2.4;% 图片预处理，加载，灰度化，转化为双精度
 width  = size( I, 2 );
 height = size( I, 1 );
 
 % regularization scale factor
-patch_size      = 11;    % size of patch to compute dictionary for
-dict_size_ratio = 1.5;  % ratio of dictionary atoms to patch DOF count
+patch_size      = 11;    % size of patch to compute dictionary for 字典块的大小 11*11
+dict_size_ratio = 1.5;  % ratio of dictionary atoms to patch DOF count 字典原子与补丁DOF计数的比率
+%  DOF是啥
+reg_weight      = 0.05;  % regularizer（调整） weight (relative to 1.2/sqrt(DOF))
+batch_size      = 512;  % size of batches（批量） for dictionary update
+iterations      = 100;   % number of dictionary update iterations（迭代次数）有点少
 
-reg_weight      = 0.05;  % regularizer weight (relative to 1.2/sqrt(DOF))
-batch_size      = 512;  % size of batches for dictionary update
-iterations      = 100;   % number of dictionary update iterations
-
-% extract some patches
+% extract（提取） some patches
 win    = round((patch_size-1)/2);
 X      = extract_patches( I, batch_size, win );
 pwidth = win+win+1;
 
-% generate an initial dictionary
+% generate an initial dictionary 生成一个初始词典
 Nd = round( pwidth*dict_size_ratio );
 D = extract_patches( I, Nd*Nd, win );
 if sum(sum(abs(isnan(D)))) > 1e-4 || sum(sum(abs(isinf(D))))
@@ -29,7 +29,8 @@ if sum(sum(abs(isnan(D)))) > 1e-4 || sum(sum(abs(isinf(D))))
 end
 
 % main dictionary computation loop, extract a new set of patches
-% and refine the current dictionary estimate
+% and refine（改进） the current dictionary estimate
+%主字典计算循环，提取一组新的补丁，并改进当前的字典估计
 A = zeros( size(D,2), size(D,2) );
 B = zeros( size(D,1), size(D,2) );
 for i=1:iterations,
@@ -46,10 +47,10 @@ fprintf( 'maximum sparse coding value: %f\n', max(alpha(:)) );
 fprintf( 'minimum dictionary value:    %f\n', min(D(:)) );
 fprintf( 'maximum dictionary value:    %f\n', max(D(:)) );
 
-% predict the final set of patches
+% predict the final set of patches 预测最终的补丁集
 pX = max( D*alpha, 0 );
 
-% rescale the patches to the range [0,1] for display
+% rescale the patches to the range [0,1] for display 归一化，方便显示
 for i=1:size(D,2),
     D(:,i) = D(:,i) - min(D(:,i));
     D(:,i) = D(:,i)/max( D(:,i) );
